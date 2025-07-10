a4b6c9333d8645502b16d6724889906b4891ed5c
$(gh api user --jq .login) <$(gh api user --jq .email)>
2025-07-10 20:09:39 +0800

    修复了上传文件路径错误的bug，与前端进行适配，增加了使用完文件后马上删除，吃了一点小蛋糕(=´ω`=)

--- backed_end/controller/ai_controller.py
+++ backed_end/controller/ai_controller.py
@@ -1,9 +1,6 @@
+import os
 from fastapi import APIRouter, Depends, Form, File, UploadFile
-from ipywidgets.embed import dependency_state
-from sqlalchemy import false
-
 from backed_end.pojo.User import User
-from backed_end.pojo.question import question
 from backed_end.service.aiservice.aiservice import service
 from backed_end.config.user_mannage import get_current_active_user
 from typing import Annotated, Optional, List
@@ -13,9 +10,13 @@ router = APIRouter()
 async def ai(question: Annotated[str,Form()],user:Annotated[User,Depends(get_current_active_user)],
              web_search:Annotated[bool,Form()]=False,files:Optional[List[UploadFile]]=File(None)):
     has_file = bool(files and len(files) > 0)
-
+    uploaded_file_paths = []
     if has_file:
         for file in files:
-            upload_service.upload_file_service(file,user.username)
-    result = service(question,user.username,web_search,has_file)
+            file_info = upload_service.upload_file_service(file,user.username)
+            uploaded_file_paths.append(file_info["filepath"])
+    result = service(question, user.username, web_search, has_file)
+    for path in uploaded_file_paths:
+        if os.path.exists(path):
+            os.remove(path)
     return {"result": result}
